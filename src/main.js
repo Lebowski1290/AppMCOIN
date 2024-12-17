3edbdf261ca7fc811758fa9f144501fd52008d36
Lebowski1290 <linkornin@gmail.com>
2024-12-17 22:55:14 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from '/node_modules/vue'
+import { createPinia } from '/node_modules/pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
